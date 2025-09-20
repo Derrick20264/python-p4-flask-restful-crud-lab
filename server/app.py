b8154f02b128153973f871300ec9aea8cc69ac90
derrick <derrick.mwangi2@student.moringaschool.com>
2025-09-20 22:37:06 +0300

    Implement GET, POST, PATCH, DELETE routes for Plant API with Flask-RESTful

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -18,23 +17,19 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
 
     def post(self):
         data = request.get_json()
-
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
             price=data['price'],
         )
-
         db.session.add(new_plant)
         db.session.commit()
-
         return make_response(new_plant.to_dict(), 201)
 
 
@@ -42,14 +37,30 @@ api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get_or_404(id)
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def patch(self, id):
+        plant = Plant.query.get_or_404(id)
+        data = request.get_json()
+
+        # Update only provided fields
+        for key, value in data.items():
+            if hasattr(plant, key):
+                setattr(plant, key, value)
+
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get_or_404(id)
+        db.session.delete(plant)
+        db.session.commit()
+        return '', 204
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
